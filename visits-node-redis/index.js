9e0324fe7d3a1f5f8fd1580a1a835fff68e04183
samroodAli <devsamrood@gmail.com>
2021-10-12 21:10:28 +0530

    Visits:Create server with express and redis

---
+++ visits-node-redis/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const redis = require('redis')
+
+const app = express()
+const client = redis.createClient()
+
+
+client.on("error", function(error) {
+  console.error(error);
+});
+
+client.set('visits',0)
+
+
+app.get("/",(req,res)=>{
+  client.get('visits',(err,visits)=>{
+    if(!err){
+      res.send(`Number of visits is ${visits}`)
+      client.set('visits',parseInt(visits) + 1)
+    }
+  })
+})
